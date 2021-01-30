9e3dc750a7ebb1d138c9f96b91c157ea443d98e4
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-01-30 16:53:54 +0800

    Add files via upload

--- configs.js
+++ configs.js
@@ -1,13 +1,13 @@
 YL.static = {
   /** “关于”信息 */
-  softwareName: 'YLUI DEMO', //网站名。请在此处填写您自己的网站名，如王小明的博客。
+  softwareName: 'YLUI Demo', //网站名。请在此处填写您自己的网站名，如王小明的博客。
   version: "1.0.0", // 网站版本号
   iconBtnStart: 'html5', //主图标
-  author: 'null',//作者
-  contactInformation: 'null',//联系方式
-  officialWebsite: '//null',//软件官网
+  author: 'CYY',//作者
+  contactInformation: '',//联系方式
+  officialWebsite: 'https://ylui.cyybt.tk:50071',//软件官网
   welcome: '本网站UI由 YLUI 强力驱动\n更多信息：//ylui.yuri2.cn',//加载完毕控制台提示信息
-  copyrightDetail: 'null',//版权详细信息
+  copyrightDetail: '',//版权详细信息
   otherStatements: '',//其他信息（可留空）
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
@@ -20,7 +20,7 @@ YL.static = {
   beforeOnloadEnable: true,//启用关闭前询问（打包app时请关闭防止出错）
   WarningPerformanceInIE: true,//在IE下提示体验不佳信息
   languages: {}, //推荐留空，自动从文件加载
-  changeable: true,//存档数据是否可被普通用户修改
+  changeable: false,//存档数据是否可被普通用户修改
   dataCenter: true,//是否展示数据管理中心
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
